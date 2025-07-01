2cdffd45f283fd50ae5f68f941d47be8b70949a4
Ibrar Ujn <sarwarujjanibrar@gmail.com>
2025-07-01 03:47:02 -0700

    new add

---
+++ codes.py
@@ -0,0 +1,7 @@
+height = input("Entre the height:").split()
+for i in range(0,len(height)):
+
+    
+    height[i]=int(height[i])
+print(height)
+    
---
+++ here.py
@@ -0,0 +1,5 @@
+j=1
+for i in range (11,111,11):
+    
+    print(f"{11}x{j}={i}")
+    j+=1
